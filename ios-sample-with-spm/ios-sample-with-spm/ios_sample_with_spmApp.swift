75e9843667166b57196a0d4452a8b632a6534157
Furkan Parlak <furkan.parlak@basarsoft.com.tr>
2025-10-30 11:20:39 +0300

    Added initial iOS sample app with Swift Package Manager (SPM)

---
+++ ios-sample-with-spm/ios-sample-with-spm/ios_sample_with_spmApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_sample_with_spmApp.swift
+//  ios-sample-with-spm
+//
+//  Created by Furkan Parlak on 30.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct ios_sample_with_spmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios-sample-with-spm/ios-sample-with-spmTests/ios_sample_with_spmTests.swift
@@ -0,0 +1,17 @@
+//
+//  ios_sample_with_spmTests.swift
+//  ios-sample-with-spmTests
+//
+//  Created by Furkan Parlak on 30.10.2025.
+//
+
+import Testing
+@testable import ios_sample_with_spm
+
+struct ios_sample_with_spmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
